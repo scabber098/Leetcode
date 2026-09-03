55d6252f6f0fdad6143b05ac68744cd30bbaac48
scabber098 <kunwardeepsingh797@gmail.com>
2026-09-03 11:35:13 +0530

    Time: 15 ms (99.35%), Space: 47.2 MB (30.56%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,76 +1,112 @@
 class Solution {
 
-    class TrieNode {
-        TrieNode[] child = new TrieNode[26];
-        String word = "";
+    static class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        int wordIndex = -1;
     }
 
     TrieNode root = new TrieNode();
-    List<String> ans = new ArrayList<>();
+    List<String> result = new ArrayList<>();
+    String[] words;
+    int rows, cols;
 
-    int m, n;
+    public List<String> findWords(char[][] board, String[] words) {
+        this.words = words;
+        rows = board.length;
+        cols = board[0].length;
 
-    void insert(String word) {
-        TrieNode node = root;
-
-        for (char c : word.toCharArray()) {
-            int idx = c - 'a';
-
-            if (node.child[idx] == null) {
-                node.child[idx] = new TrieNode();
-            }
-
-            node = node.child[idx];
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i], i);
         }
 
-        node.word = word;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                dfs(board, r, c, root);
+            }
+        }
+
+        return result;
     }
 
-    void dfs(char[][] board, int r, int c, TrieNode node) {
+    private void insert(String word, int index) {
+        TrieNode node = root;
 
-        if (r < 0 || c < 0 || r >= m || c >= n)
-            return;
+        for (int i = 0; i < word.length(); i++) {
+            int x = word.charAt(i) - 'a';
+
+            if (node.next[x] == null) {
+                node.next[x] = new TrieNode();
+            }
+
+            node = node.next[x];
+        }
+
+        node.wordIndex = index;
+    }
+
+    private void dfs(char[][] board, int r, int c, TrieNode parent) {
 
         char ch = board[r][c];
 
         if (ch == '#')
             return;
 
-        TrieNode next = node.child[ch - 'a'];
+        TrieNode node = parent.next[ch - 'a'];
 
-        if (next == null)
+        if (node == null)
             return;
 
-        if (!next.word.equals("")) {
-            ans.add(next.word);
-            next.word = "";
+        if (node.wordIndex != -1) {
+            result.add(words[node.wordIndex]);
+            node.wordIndex = -1;
         }
 
         board[r][c] = '#';
 
-        dfs(board, r + 1, c, next);
-        dfs(board, r - 1, c, next);
-        dfs(board, r, c + 1, next);
-        dfs(board, r, c - 1, next);
+        if (r > 0)
+            dfs(board, r - 1, c, node);
+
+        if (r + 1 < rows)
+            dfs(board, r + 1, c, node);
+
+        if (c > 0)
+            dfs(board, r, c - 1, node);
+
+        if (c + 1 < cols)
+            dfs(board, r, c + 1, node);
 
         board[r][c] = ch;
-    }
 
-    public List<String> findWords(char[][] board, String[] words) {
+        // Trie pruning
+        if (node.wordIndex == -1 &&
+            node.next[0] == null &&
+            node.next[1] == null &&
+            node.next[2] == null &&
+            node.next[3] == null &&
+            node.next[4] == null &&
+            node.next[5] == null &&
+            node.next[6] == null &&
+            node.next[7] == null &&
+            node.next[8] == null &&
+            node.next[9] == null &&
+            node.next[10] == null &&
+            node.next[11] == null &&
+            node.next[12] == null &&
+            node.next[13] == null &&
+            node.next[14] == null &&
+            node.next[15] == null &&
+            node.next[16] == null &&
+            node.next[17] == null &&
+            node.next[18] == null &&
+            node.next[19] == null &&
+            node.next[20] == null &&
+            node.next[21] == null &&
+            node.next[22] == null &&
+            node.next[23] == null &&
+            node.next[24] == null &&
+            node.next[25] == null) {
 
-        m = board.length;
-        n = board[0].length;
-
-        for (String word : words) {
-            insert(word);
+            parent.next[ch - 'a'] = null;
         }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dfs(board, i, j, root);
-            }
-        }
-
-        return ans;
     }
 }
